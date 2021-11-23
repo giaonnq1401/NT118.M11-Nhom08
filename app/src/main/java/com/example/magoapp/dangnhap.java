8b46ce6193e61788a5a15a2f04d83b4f63d06e97
Nguyễn Ngọc Quỳnh Giao <giaonguyennq142001@gmail.com>
2021-11-23 21:43:47 +0700

    splash screen

--- app/src/main/java/com/example/magoapp/dangnhap.java
+++ app/src/main/java/com/example/magoapp/dangnhap.java
@@ -3,6 +3,7 @@ package com.example.magoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class dangnhap extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class dangnhap extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dangnhap);
+
+        //go to homepage
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
     }
 }
