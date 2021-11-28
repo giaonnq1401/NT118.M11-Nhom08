19661aeae3a8353bea903447a6ea173074f160ca
Nguyễn Ngọc Quỳnh Giao <giaonguyennq142001@gmail.com>
2021-11-28 08:46:33 +0700

    login update

--- app/src/main/java/com/example/magoapp/dangnhap.java
+++ app/src/main/java/com/example/magoapp/dangnhap.java
@@ -2,10 +2,15 @@ package com.example.magoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class dangnhap extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,16 @@ public class dangnhap extends AppCompatActivity {
         setContentView(R.layout.activity_dangnhap);
 
         //go to homepage
-        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+
+        //Register
+        btnSignup = (Button) findViewById(R.id.btn_signup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(dangnhap.this, dangki.class));
+            }
+        });
     }
 }
