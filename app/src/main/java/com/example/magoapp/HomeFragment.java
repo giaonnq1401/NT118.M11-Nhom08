b2f57ce8f1cd2d5e8556515305e7fe4e8f479a67
Nguyễn Ngọc Quỳnh Giao <giaonguyennq142001@gmail.com>
2021-12-10 15:53:58 +0700

    update

--- app/src/main/java/com/example/magoapp/HomeFragment.java
+++ app/src/main/java/com/example/magoapp/HomeFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +16,7 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private TextView textViewTitle;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -61,4 +63,5 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
 }
