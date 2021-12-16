a4e594d400a3cbfffedca7f7d37dfc418b2f9a0d
19520538 <19520538@gm.uit.edu.vn>
2021-12-16 15:19:36 +0700

    story_page

---
+++ app/src/main/java/com/example/magoapp/activity_story.java
@@ -0,0 +1,14 @@
+package com.example.magoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_story extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+    }
+}
